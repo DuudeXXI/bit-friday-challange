56ad40cebc26bd69d1e36ff078741b55b63a3c9f
DuudeXXI <106966677+DuudeXXI@users.noreply.github.com>
2022-07-22 12:29:03 +0300

    making button stay active

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const buttonDOM = document.querySelectorAll('.button-dom');
+
+for (let i = 0; i < buttonDOM.length; i++) {
+    let button = buttonDOM[i]
+    button.addEventListener('click', function () {
+            console.log('button cliked');
+            button.classList.toggle('active');
+    });
+    
+}
